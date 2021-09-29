ed36ff7dc3b4f056985b1513aac6f6266dfd850e
Vadim Polyakov <cachegetfield@yandex.ru>
2021-09-29 19:46:45 +0300

    Добавлены аннотации в HTTPRequest

--- src/Parser/HTTPRequest.php
+++ src/Parser/HTTPRequest.php
@@ -7,29 +7,60 @@ use http\Encoding\Stream\Inflate;
 
 class HTTPRequest
 {
+    /** @var string Storage of CODE */
     protected string $code;
+
+    /** @var string Storage of SIZE */
     protected string $size;
+
+    /** @var string Storage of URL */
     protected string $url;
+
+    /** @var string Storage of USERAGENT */
     protected string $userAgent;
 
+    /**
+     * Method for setting $code
+     *
+     * @param string $code
+     * @return $this
+     */
     public function code(string $code): self
     {
         $this->code = $code;
         return $this;
     }
 
+    /**
+     * Method for setting $size
+     *
+     * @param string $size
+     * @return $this
+     */
     public function size(string $size): self
     {
         $this->size = $size;
         return $this;
     }
 
+    /**
+     * Method for setting $url
+     *
+     * @param string $url
+     * @return $this
+     */
     public function url(string $url): self
     {
         $this->url = $url;
         return $this;
     }
 
+    /**
+     * Method for setting $userAgent
+     *
+     * @param string $userAgent
+     * @return $this
+     */
     public function userAgent(string $userAgent): self
     {
         $this->userAgent = $userAgent;
@@ -37,6 +68,8 @@ class HTTPRequest
     }
 
     /**
+     * Method for getting $size
+     *
      * @return string
      */
     public function getSize(): string
@@ -45,6 +78,8 @@ class HTTPRequest
     }
 
     /**
+     * Method for getting $code
+     *
      * @return string
      */
     public function getCode(): string
@@ -53,6 +88,8 @@ class HTTPRequest
     }
 
     /**
+     * Method for getting $url
+     *
      * @return string
      */
     public function getUrl(): string
@@ -61,6 +98,8 @@ class HTTPRequest
     }
 
     /**
+     * Method for getting $userAgent
+     *
      * @return string
      */
     public function getUserAgent(): string
