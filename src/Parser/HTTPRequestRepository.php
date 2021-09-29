e444bbd9aaab07f9b8266c38fe789c3ea3125b93
Vadim Polyakov <cachegetfield@yandex.ru>
2021-09-29 19:48:08 +0300

    Добавлены аннотации для HTTPRequestRepository

--- src/Parser/HTTPRequestRepository.php
+++ src/Parser/HTTPRequestRepository.php
@@ -4,13 +4,24 @@ namespace App\Parser;
 
 class HTTPRequestRepository
 {
+    /** @var array Local storage (repository) */
     protected array $logs = [];
 
+    /**
+     * Saving an HTTPRequest instance
+     *
+     * @param HTTPRequest $request
+     */
     public function save(HTTPRequest $request)
     {
         $this->logs[] = $request;
     }
 
+    /**
+     * Method for getting all request from array (repository)
+     *
+     * @return array
+     */
     public function getRequests(): array
     {
         return $this->logs;
